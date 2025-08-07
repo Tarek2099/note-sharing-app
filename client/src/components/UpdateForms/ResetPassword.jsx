f90dc1e036e7382520ce895e668ddd758968aee8
MD Tarek Rahman <mdtarekrahman209@gmail.com>
2025-08-07 21:14:32 +0600

    feat: Implement reset password functionality by sending reset link

--- client/src/components/UpdateForms/ResetPassword.jsx
+++ client/src/components/UpdateForms/ResetPassword.jsx
@@ -1,19 +1,54 @@
 import { useState } from "react"
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import app from "../../firebase/firebase.config";
 
+const auth = getAuth(app);
 const ResetPassword = () => {
-    const [oldPassword, setOldPassword] = useState("");
-    const [newPassword, setNewPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [isSuccess, setIsSuccess] = useState(false);
+
+    // Reset Password Function 
+    const resetHandler = async (e) => {
+        e.preventDefault();
+        if (!email) {
+            setMessage("Please enter your email");
+        }
+        try {
+            await sendPasswordResetEmail(auth, email)
+                .then(() => {
+                    setMessage("Password reset email sent");
+                    setIsSuccess(true);
+                    setEmail("");
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    setMessage(errorMessage);
+                    setIsSuccess(false);
+                });
+        } catch {
+            console.log("Failed to send reset email");
+        }
+    }
     return (
         <div className="max-w-full md:w-md p-4 rounded shadow">
             <h1 className="text-2xl font-bold text-center">Reset Your Password</h1>
-            <form action="submit" className="space-y-4 my-5 p-4">
+            <form action="submit"
+                className="space-y-4 my-5 p-4"
+                onSubmit={resetHandler}
+            >
                 <div>
-                    <label htmlFor="oldPassword" className="block text-sm font-medium mb-2">Old Password</label>
-                    <input type="password" placeholder="Old Password" className="w-full rounded outline-none p-2 bg-gray-100" value={oldPassword} />
-                </div>
-                <div>
-                    <label htmlFor="newPassword" className="block text-sm font-medium mb-2">New Password</label>
-                    <input type="password" placeholder="New Password" className="w-full rounded outline-none p-2 bg-gray-100" value={newPassword} />
+
+                    {message && <p className={`p-2 w-full mb-2 text-center ${isSuccess ? "text-green-600 bg-green-100" : "text-red-600 bg-red-100"}`}>{message}</p>}
+                    <label htmlFor="oldPassword" className="block text-sm font-medium mb-2">Email Address</label>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        className="w-full rounded outline-none p-2 bg-gray-100"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                 </div>
                 <button type="submit" className="w-full py-2 bg-blue-600 text-white rounded cursor-pointer">Reset Password</button>
             </form>
